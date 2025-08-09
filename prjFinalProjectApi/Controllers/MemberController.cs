21bf1b65d48d961e109de6eec795ef3081b9a34b
謝維澤 <xie8610@gmail.com>
2025-08-09 16:59:17 +0800

    公版

---
+++ prjFinalProjectApi/Controllers/MemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjFinalProjectApi.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
